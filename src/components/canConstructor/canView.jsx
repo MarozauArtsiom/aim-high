45e2e97f47eeac597a46fb736bdf85cf3868c99c
Artsiom.Marozau <artsiom.marozau@ukg.com>
2024-05-17 21:55:42 +0200

    feat: apply new soda

--- src/components/canConstructor/canView.jsx
+++ src/components/canConstructor/canView.jsx
@@ -1,7 +1,9 @@
 import "./canView.css";
 import CanBackground from "./canBackground";
 import { CAN_COLOR_LABEL_MAP } from "./../const";
+
 import shadowsImage from "../../assets/images/soda/shadows.png";
+import canWaterImage from "../../assets/images/soda/can_water.png";
 
 import edgeBlack from "../../assets/images/soda/can_top_bottom_black.png";
 import edgeGold from "../../assets/images/soda/can_top_bottom_gold.png";
@@ -35,6 +37,12 @@ export default function CanView({ canColor, stickerColor }) {
         alt="Soda Shadows"
         className="soda-layer shadows-layer"
       />
+      <img
+        src={canWaterImage}
+        aria-hidden
+        alt="Soda Water"
+        className="soda-layer can-water-layer"
+      ></img>
       <div
         className="place-holder-to-keep-object-size"
         style={{ width: 600, height: 600 }}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@
 import "./assets/ClashDisplay_Complete/Fonts/WEB/css/clash-display.css";
 import "./assets/ClashGrotesk_Complete/Fonts/WEB/css/clash-grotesk.css";
 
-// styles
-import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
