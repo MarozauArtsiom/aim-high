d76b5e7e14faad586e4d1439b5f6c1c8c45abd34
Artsiom.Marozau <artsiom.marozau@ukg.com>
2024-05-21 21:33:50 +0200

    feat: draggable popover

--- src/components/imageCropper/imageCropper.jsx
+++ src/components/imageCropper/imageCropper.jsx
@@ -8,6 +8,7 @@ import { useCreateObjectUrl } from "../../hooks/image";
 import CloseIcon from "@mui/icons-material/Close";
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import Draggable from "react-draggable";
 
 const ImageCropper = ({
   file,
@@ -71,96 +72,98 @@ const ImageCropper = ({
   };
 
   return (
-    <Popover
-      open={isOpen}
-      onClose={onClose}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "center",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "center",
-      }}
-      slotProps={{
-        paper: {
-          style: {
-            maxWidth: fullScreen ? "100vw" : "50vw",
-            maxHeight: fullScreen ? "100vh" : "70vh",
-            padding: 20,
-            zIndex: 100500,
+    <Draggable>
+      <Popover
+        open={isOpen}
+        onClose={onClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        slotProps={{
+          paper: {
+            style: {
+              maxWidth: fullScreen ? "100vw" : "50vw",
+              maxHeight: fullScreen ? "100vh" : "70vh",
+              padding: 20,
+              zIndex: 100500,
+            },
           },
-        },
-      }}
-    >
-      {imageSrc && (
-        <div className="c-image-cropper-container">
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            marginBottom={1}
-          >
-            <Typography variant="h6" component="div">
-              Position: {file?.name || "unknown file name"}
-            </Typography>
-            <Box xs={{ gap: "5px" }}>
-              <TextField
-                size="small"
-                label="X"
-                name="x"
-                type="number"
-                value={imageRect.x}
-                onChange={handleChange}
-                style={{ width: 60, marginRight: 8 }}
-              />
-              <TextField
-                size="small"
-                label="Y"
-                name="y"
-                type="number"
-                value={imageRect.y}
-                onChange={handleChange}
-                style={{ width: 60, marginRight: 8 }}
-              />
-              <TextField
-                size="small"
-                label="Width"
-                name="width"
-                type="number"
-                value={imageRect.width}
-                onChange={handleChange}
-                style={{ width: 60, marginRight: 8 }}
-              />
-              <TextField
-                size="small"
-                label="Height"
-                name="height"
-                type="number"
-                value={imageRect.height}
-                onChange={handleChange}
-                style={{ width: 60, marginRight: 8 }}
-              />
+        }}
+      >
+        {imageSrc && (
+          <div className="c-image-cropper-container">
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              marginBottom={1}
+            >
+              <Typography variant="h6" component="div">
+                Position: {file?.name || "unknown file name"}
+              </Typography>
+              <Box xs={{ gap: "5px" }}>
+                <TextField
+                  size="small"
+                  label="X"
+                  name="x"
+                  type="number"
+                  value={imageRect.x}
+                  onChange={handleChange}
+                  style={{ width: 60, marginRight: 8 }}
+                />
+                <TextField
+                  size="small"
+                  label="Y"
+                  name="y"
+                  type="number"
+                  value={imageRect.y}
+                  onChange={handleChange}
+                  style={{ width: 60, marginRight: 8 }}
+                />
+                <TextField
+                  size="small"
+                  label="Width"
+                  name="width"
+                  type="number"
+                  value={imageRect.width}
+                  onChange={handleChange}
+                  style={{ width: 60, marginRight: 8 }}
+                />
+                <TextField
+                  size="small"
+                  label="Height"
+                  name="height"
+                  type="number"
+                  value={imageRect.height}
+                  onChange={handleChange}
+                  style={{ width: 60, marginRight: 8 }}
+                />
+              </Box>
+              <IconButton onClick={onClose}>
+                <CloseIcon />
+              </IconButton>
             </Box>
-            <IconButton onClick={onClose}>
-              <CloseIcon />
-            </IconButton>
-          </Box>
-          <ReactCrop
-            crop={crop}
-            onChange={(newCrop) => setCrop(newCrop)}
-            onComplete={onCropComplete}
-          >
-            <img
-              ref={imageRef}
-              alt="Crop me"
-              src={imageSrc}
-              className="c-image-cropper-container__image"
-            />
-          </ReactCrop>
-        </div>
-      )}
-    </Popover>
+            <ReactCrop
+              crop={crop}
+              onChange={(newCrop) => setCrop(newCrop)}
+              onComplete={onCropComplete}
+            >
+              <img
+                ref={imageRef}
+                alt="Crop me"
+                src={imageSrc}
+                className="c-image-cropper-container__image"
+              />
+            </ReactCrop>
+          </div>
+        )}
+      </Popover>
+    </Draggable>
   );
 };
 
